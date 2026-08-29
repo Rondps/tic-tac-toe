deb2af4b1c354515a65c95f9d85ab4c8310adc68
R.Pinheiro <rondy.nelly.rp@gmail.com>
2026-08-29 16:46:52 -0300

    Corrigir asserção do teste do bot

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,73 +1,95 @@
 from app import check_winner, bot_move, reset_game
 
-
 def test_check_winner_horizontal_x():
-    board = [["X", "X", "X"], ["", "", ""], ["", "", ""]]
-    assert check_winner(board) == "X"
-
+    board = [
+        ['X', 'X', 'X'],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
+    assert check_winner(board) == 'X'
 
 def test_check_winner_horizontal_o():
-    board = [["", "", ""], ["O", "O", "O"], ["", "", ""]]
-    assert check_winner(board) == "O"
-
+    board = [
+        ['O', 'O', 'O'],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
+    assert check_winner(board) == 'O'
 
 def test_check_winner_vertical_x():
-    board = [["X", "", ""], ["X", "", ""], ["X", "", ""]]
-    assert check_winner(board) == "X"
-
+    board = [
+        ['X', ' ', ' '],
+        ['X', ' ', ' '],
+        ['X', ' ', ' ']
+    ]
+    assert check_winner(board) == 'X'
 
 def test_check_winner_vertical_o():
-    board = [["", "O", ""], ["", "O", ""], ["", "O", ""]]
-    assert check_winner(board) == "O"
-
+    board = [
+        [' ', 'O', ' '],
+        [' ', 'O', ' '],
+        [' ', 'O', ' ']
+    ]
+    assert check_winner(board) == 'O'
 
 def test_check_winner_diagonal_x():
-    board = [["X", "", ""], ["", "X", ""], ["", "", "X"]]
-    assert check_winner(board) == "X"
-
+    board = [
+        ['X', ' ', ' '],
+        [' ', 'X', ' '],
+        [' ', ' ', 'X']
+    ]
+    assert check_winner(board) == 'X'
 
 def test_check_winner_diagonal_o():
-    board = [["", "", "O"], ["", "O", ""], ["O", "", ""]]
-    assert check_winner(board) == "O"
-
+    board = [
+        [' ', ' ', 'O'],
+        [' ', 'O', ' '],
+        ['O', ' ', ' ']
+    ]
+    assert check_winner(board) == 'O'
 
 def test_check_winner_draw():
-    board = [["X", "O", "X"], ["O", "X", "O"], ["O", "X", "O"]]
-    assert check_winner(board) == "draw"
-
+    board = [
+        ['X', 'O', 'X'],
+        ['X', 'O', 'O'],
+        ['O', 'X', 'X']
+    ]
+    assert check_winner(board) == 'Draw'
 
 def test_check_winner_incomplete():
-    board = [["X", "", ""], ["", "", ""], ["", "", ""]]
+    board = [
+        ['X', 'O', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
     assert check_winner(board) is None
 
-
 def test_bot_move():
-    board = [["X", "", ""], ["", "O", ""], ["", "", ""]]
+    board = [
+        ['X', ' ', ' '],
+        [' ', 'O', ' '],
+        [' ', ' ', ' ']
+    ]
     result = bot_move(board)
-    assert result in [(0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1)]
-
+    assert result in [(0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2)]
 
 def test_bot_move_no_empty():
-    board = [["X", "O", "X"], ["O", "X", "O"], ["O", "X", "O"]]
-    result = bot_move(board)
-    assert result is None
-
-
-class MockState:
-    def __init__(self):
-        self.board = [["X", "O", "X"], ["O", "X", "O"], ["O", "X", "O"]]
-        self.current_player = "O"
-        self.game_over = True
-        self.winner = "X"
-        self.scores = {"X": 5, "O": 3, "draw": 2}
-        self.board_key = 10
-
+    board = [
+        ['X', 'O', 'X'],
+        ['X', 'O', 'O'],
+        ['O', 'X', 'X']
+    ]
+    assert bot_move(board) is None
 
 def test_reset_game():
-    state = MockState()
-    reset_game(state)
-    assert state.board == [["", "", ""] for _ in range(3)]
-    assert state.current_player == "X"
-    assert state.game_over is False
-    assert state.winner is None
-    assert state.board_key == 11
+    board = [
+        ['X', 'O', 'X'],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
+    new_board = reset_game()
+    assert new_board == [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
